696b8d86aac59b214d0e09fe410e7b22cd220c05
evgeniyparinov <evgenii.parinov@gmail.com>
2020-09-03 20:17:59 +0300

    add new files locators.py, login_page.py; modify main_page.py; add test should_be_login_page to test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,3 +8,5 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
