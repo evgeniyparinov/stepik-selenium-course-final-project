aed06443ab9a47c7d7a98435d208216f212b635a
evgeniyparinov <evgenii.parinov@gmail.com>
2020-09-04 17:14:57 +0300

    Add login_page's initialization code in test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 from pages.main_page import MainPage
-from pages.login_page import LoginPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,6 +7,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
